3eadfbb41f92b42bfebcf56634848cc96f3420e2
fable54321 <timothe5@hotmail.com>
2026-05-07 07:56:44 -0400

    first commit

--- src/100--App/App.tsx
+++ src/100--App/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <article className="flex flex-col items-center font-secondary w-full">
      
-      <h1 className="mt-10 font-primary  rounded-lg text-black text-[3em] font-bold">  
+      <h1 className="mt-10 font-primary text-center  rounded-lg text-black text-[3em] font-bold">  
 Inventario de cajas de herramientas</h1>
 
       <Outlet />
