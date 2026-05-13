3380835754d830dca5eccc09d115d24812ddecb2
fable54321 <timothe5@hotmail.com>
2026-05-13 06:37:14 -0400

    first commit

---
+++ src/Components/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+import { usePersistentAuth } from "../Contexts/AuthContext/AuthContext";
+import { useEffect } from "react";
+import type { JSX } from "react";
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { user, loading } = usePersistentAuth();
+
+  const hasAccess = !!user?.appAccess?.some((app) => app.slug === "toolbox");
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (!user || !hasAccess) {
+      alert("Vous n'avez pas les permissions nécessaires pour accéder à cette application.");
+      window.location.replace("https://vegibec-portail.com/");
+      return;
+    }
+
+    
+  }, [user, loading, hasAccess]);
+
+  if (loading) return <div>Chargement...</div>;
+
+  if (!user || !hasAccess) return null;
+
+  return children;
+};
+
+
+
+export default ProtectedRoute;
