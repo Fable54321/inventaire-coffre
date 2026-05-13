025aeba9846babaa6be76d5f446cbb867c4cc5ba
fable54321 <timothe5@hotmail.com>
2026-05-13 06:53:25 -0400

    first commit

--- src/100--App/App.tsx
+++ src/100--App/App.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom"
+import { usePersistentAuth } from "../Contexts/AuthContext/AuthContext";
+import { useEffect } from "react";
 
 
 
 function App() {
 
 
+  const { user, } = usePersistentAuth();
+
+
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+    }
+  }, [user]);
 
   return (
     <article className="flex flex-col items-center font-secondary w-full">
