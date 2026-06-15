23950f3dc4a753e03f0b6638f259a216cd918467
fable54321 <timothe5@hotmail.com>
2026-06-15 10:46:17 -0400

    first commit

--- src/170--ToolboxPictures/ToolboxPictures.tsx
+++ src/170--ToolboxPictures/ToolboxPictures.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useToolBoxes } from "../Contexts/ToolBoxesContext/UseToolBoxes";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
+import type { ToolBox } from "../Contexts/ToolBoxesContext/ToolBoxesContext";
 
 
 const ToolboxPictures = () => {
@@ -8,7 +9,7 @@ const ToolboxPictures = () => {
 
 const { toolboxId } = useParams();
 
-const { fetchToolboxPictures, toolboxPictures } = useToolBoxes();
+const { fetchToolboxPictures, toolboxPictures, toolBoxes } = useToolBoxes();
 
 
 useEffect(() => {
@@ -23,20 +24,31 @@ useEffect(() => {
 }, [toolboxPictures]);
 
 
+const selectedToolbox = useMemo(() => {
+
+if (!toolBoxes || !toolboxId) return null;
+
+  return toolBoxes.find((toolbox : ToolBox) => toolbox.id === Number(toolboxId));
+}, [toolBoxes, toolboxId]);
+
+
   return (
     <article className="w-full flex flex-col items-center">
-    <div className="grid grid-cols-2 items-center gap-2 w-[min(750px,calc(100%-1rem))]" >
+        <h2>Fotos de la caja {selectedToolbox?.code}</h2>
+    <div className="mt-4 md:grid md:grid-cols-3 flex flex-col  items-center gap-2 gap-y-10 w-[min(750px,calc(100%-1rem))]" >
       {toolboxPictures.map((picture) => (
         <>
        
-        <div key={picture.id} className="  mx-auto">
+        <div key={picture.id} className="  mx-auto mt-0 mb-auto border border-b border-black/10 bg-[#f4fdf1] rounded-xl shadow-2xl">
         
-        <div className="flex flex-col">
-          <p>{picture.description}</p>        
+        <div className="flex flex-col gap-3 relative">
+          <p className="flex flex-col justify-center min-h-20 text-center text-[1.1em] font-bold max-w-[98%]">{picture.description}</p>        
           <img src={picture.signed_url} alt={picture.description || ""} />
           </div>
+          
         </div>
         </>
+        
       ))}
 
     </div>
