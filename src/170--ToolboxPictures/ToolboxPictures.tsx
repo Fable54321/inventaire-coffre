05406985d46666d5c1a5c0e3486901b12815e953
fable54321 <timothe5@hotmail.com>
2026-06-15 10:06:46 -0400

    first commit

--- src/170--ToolboxPictures/ToolboxPictures.tsx
+++ src/170--ToolboxPictures/ToolboxPictures.tsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { useToolBoxes } from "../Contexts/ToolBoxesContext/UseToolBoxes";
+import { useEffect } from "react";
 
 
 const ToolboxPictures = () => {
@@ -6,14 +8,41 @@ const ToolboxPictures = () => {
 
 const { toolboxId } = useParams();
 
+const { fetchToolboxPictures, toolboxPictures } = useToolBoxes();
+
+
+useEffect(() => {
+    if (toolboxId) {
+        fetchToolboxPictures(Number(toolboxId));
+    }
+}, [fetchToolboxPictures, toolboxId]);
+
+
+useEffect(() => {
+    console.log(toolboxPictures);
+}, [toolboxPictures]);
 
 
   return (
-    <div>
-      here will be the pictures for toolbox {toolboxId}
+    <article className="w-full flex flex-col items-center">
+    <div className="grid grid-cols-2 items-center gap-2 w-[min(750px,calc(100%-1rem))]" >
+      {toolboxPictures.map((picture) => (
+        <>
+       
+        <div key={picture.id} className="  mx-auto">
+        
+        <div className="flex flex-col">
+          <p>{picture.description}</p>        
+          <img src={picture.signed_url} alt={picture.description || ""} />
+          </div>
+        </div>
+        </>
+      ))}
 
     </div>
+</article>
   )
+  
 }
 
 export default ToolboxPictures
